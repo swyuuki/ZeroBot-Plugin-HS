87fc4406d2b518e705f0be78f01168084b1e2b47
散无友纪 <tdf261939@gmail.com>
2021-08-20 00:41:54 +0800

    Update main.go

--- main.go
+++ main.go
@@ -5,23 +5,25 @@ import (
 	"os"
 	"strings"
 
-	_ "github.com/FloatTech/ZeroBot-Plugin/plugin_imagemagick" // 基础词库
+	_ "github.com/tdf1939/ZeroBot-Plugin-HS/hs"
+
 
 	// 以下为内置依赖，勿动
-	zero "github.com/wdvxdr1123/ZeroBot"
-	"github.com/wdvxdr1123/ZeroBot/driver"
-	"github.com/wdvxdr1123/ZeroBot/message"
-
 	log "github.com/sirupsen/logrus"
 	easy "github.com/t-tomalak/logrus-easy-formatter"
+	zero "github.com/wdvxdr1123/ZeroBot"
+	"github.com/wdvxdr1123/ZeroBot/driver"
 )
 
-var content = []string{
-	"* OneBot + ZeroBot + Golang ",
-	"* Version 1.0.4 - 2021-07-14 14:09:58.581489207 +0800 CST",
-	"* Copyright © 2020 - 2021  Kanri, DawnNights, Fumiama, Suika",
-	"* Project: https://github.com/FloatTech/ZeroBot-Plugin",
-}
+var (
+	contents = []string{
+		"* OneBot + ZeroBot + Golang ",
+		"* Version 1.1.0 - 2021-08-06 23:36:29 +0800 CST",
+		"* Copyright © 2020 - 2021  Kanri, DawnNights, Fumiama, Suika",
+		"* Project: https://github.com/FloatTech/ZeroBot-Plugin",
+	}
+	banner = strings.Join(contents, "\n")
+)
 
 func init() {
 	log.SetFormatter(&easy.Formatter{
@@ -33,9 +35,9 @@ func init() {
 
 func main() {
 	fmt.Print(
-		"====================[ZeroBot-Plugin]====================",
-		"\n", strings.Join(content, "\n"), "\n",
-		"========================================================",
+		"\n======================[ZeroBot-Plugin]======================",
+		"\n", banner, "\n",
+		"============================================================\n",
 	) // 启动打印
 	zero.Run(zero.Config{
 		NickName:      []string{"椛椛", "ATRI", "atri", "亚托莉", "アトリ"},
@@ -44,23 +46,21 @@ func main() {
 		// SuperUsers 某些功能需要主人权限，可通过以下两种方式修改
 		// []string{}：通过代码写死的方式添加主人账号
 		// os.Args[1:]：通过命令行参数的方式添加主人账号
-		SuperUsers: append([]string{"2031605105"}, os.Args[1:]...),
+		SuperUsers: append([]string{"12345678", "87654321"}, os.Args[1:]...),
 
 		Driver: []zero.Driver{
 			&driver.WSClient{
 				// OneBot 正向WS 默认使用 6700 端口
-				Url:         "ws://101.91.230.253:16700",
-				AccessToken: "124816",
+				Url:         "ws://127.0.0.1:6700",
+				AccessToken: "",
 			},
 		},
 	})
 
 	// 帮助
-	zero.OnFullMatchGroup([]string{"help", "/help", ".help", "菜单", "帮助"}, zero.OnlyToMe).SetBlock(false).SetPriority(999).
+	zero.OnFullMatchGroup([]string{"help", "/help", ".help", "菜单", "帮助"}, zero.OnlyToMe).SetBlock(true).SetPriority(999).
 		Handle(func(ctx *zero.Ctx) {
-			ctx.SendChain(
-				message.Text(strings.Join(content, "\n")),
-			)
+			ctx.Send(banner)
 		})
 	select {}
 }
